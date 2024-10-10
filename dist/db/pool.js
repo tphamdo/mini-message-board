56eab0e4fc791b88e3523ef801edbb0e23319853
Trueman Phamdo <trueman@phamdo.com>
2024-10-10 18:29:29 -0400

    Run build

--- dist/db/pool.js
+++ dist/db/pool.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const pg_1 = require("pg");
+console.log('database_url:', process.env.DATABASE_URL);
 exports.default = new pg_1.Pool({
     connectionString: process.env.DATABASE_URL,
 });
